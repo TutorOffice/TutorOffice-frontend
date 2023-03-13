d5a1b376284c35b2d62630dd66926dc07dff8fde
Maksim Chuchuk <cucukmaksim@gmail.com>
2023-03-13 13:20:18 +0300

    initial commit

---
+++ src/Globals.d.ts
@@ -0,0 +1,3 @@
+declare module '*.module.css';
+// and so on for whatever flavor of css you're using
+
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './components/app/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);

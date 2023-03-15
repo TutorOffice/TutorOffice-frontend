ec0ca5409d26307217f54d47b7a452eb5c9439d9
Maksim Chuchuk <cucukmaksim@gmail.com>
2023-03-15 15:33:01 +0300

    feat: сверстал главную страницу

--- src/Globals.d.ts
+++ src/Globals.d.ts
@@ -1,3 +1,5 @@
 declare module '*.module.css';
+declare module '*.jpg';
+declare module '*.png';
+declare module '*.svg';
 // and so on for whatever flavor of css you're using
-
---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import s from './Layout.module.css';
+import Navbar from '../navbar/Navbar';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className={s.layout}>
+      <Navbar />
+      <main className={s.main}>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;

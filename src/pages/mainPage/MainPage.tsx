96fb8bb360c03644b2cdc5abbd2530a46186b65d
zeroqs <smthq@bk.ru>
2023-03-17 15:13:53 +0300

    пофиксил конфликт

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -7,7 +7,6 @@ import React, { useState } from 'react';
 import Modal from '../../components/modal/Modal';
 
 const MainPage = () => {
-  const [isModal, setIsModal] = useState(false);
   return (
     <Layout>
       <section className={s.main}>
@@ -36,19 +35,6 @@ const MainPage = () => {
         </div>
         <img className={s.main__image} src={mainImage} alt='Репетитор и ученик' />
       </section>
-      <button onClick={() => setIsModal(true)}>open modal</button>
-      <Modal
-        width='570px'
-        title='Загрузка новой фотографии'
-        btnText='Выбрать файл'
-        isOpen={isModal}
-        onClose={() => setIsModal(false)}
-      >
-        <p>
-          Преподавателям будет проще узнать вас, если вы загрузите свою настоящую фотографию. Вы можете загрузить
-          изображение в формате JPG, GIF или PNG.
-        </p>
-      </Modal>
     </Layout>
   );
 };

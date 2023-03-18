a6a36354b7017232687c009aae2ef5825ecf0aa8
Nikita <94167692+zeroqs@users.noreply.github.com>
2023-03-18 13:28:13 +0300

    Feature/UI (#28)
    * доработал ui

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,25 +1,21 @@
-import { Link } from 'react-router-dom';
 import Layout from '../../components/layout/Layout';
 import s from './MainPage.module.css';
 import mainImage from '../../assets/images/main-image.png';
-import { btnClass, btnType, Button } from '../../shared/ui';
-import React from 'react';
+import { btnClass, Button } from '../../shared';
+import Input from '../../shared/ui/input/Input';
+import { btnType } from '../../shared/ui/button/Button';
 
 const MainPage = () => {
   return (
     <Layout>
       <section className={s.main}>
         <div className={s.main__buttons}>
-          <Link to='login'>
-            <Button type={btnType.button} variant={btnClass.ghost}>
-              Вход
-            </Button>
-          </Link>
-          <Link to='/register'>
-            <Button type={btnType.button} variant={btnClass.primary}>
-              Регистрация
-            </Button>
-          </Link>
+          <Button type={btnType.button} variant={btnClass.ghost}>
+            Вход
+          </Button>
+          <Button type={btnType.button} variant={btnClass.primary}>
+            Регистрация
+          </Button>
         </div>
         <div className={s.main__content}>
           <h1 className={s.main__title}>Добро пожаловать в Личный кабинет «Репетитор»!</h1>
@@ -27,6 +23,7 @@ const MainPage = () => {
             Как репетитор или ученик ты можешь составлять здесь расписание, отправлять домашние работы или хранить
             информацию и многое другое.
           </p>
+
           <Button type={btnType.button} variant={btnClass.primary}>
             Попробовать <br />
             (без регистрации)

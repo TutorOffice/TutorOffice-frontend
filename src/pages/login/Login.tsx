5dd241317a304fe1cb20e7b9daa46bdd0d7b92a4
Evgeny Abramov <abramovea@gmail.com>
2023-03-28 21:27:03 +0300

    сверстал страницу входа

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,40 @@
 import s from './Login.module.css';
+import Layout from '../../components/layout/Layout';
+import SubmitForm from '../../components/submitForm/SubmitForm';
+import { btnClass, btnType, Button, Input, inputTypes } from '../../shared/ui';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return <div>LoginPage</div>;
+  return (
+    <Layout>
+      <Button type={btnType.button} variant={btnClass.back} bottom={200}>
+        Назад
+      </Button>
+      <h2 className={s.login__title}>Вход в личный кабинет</h2>
+      <SubmitForm>
+        <Input type={inputTypes.email} labelText='Введите e-mail' isRequired={true} />
+        <Input type={inputTypes.password} labelText='Введите пароль' isRequired={true} isPassword={true} />
+        <Button type={btnType.submit} variant={btnClass.primary}>
+          Войти
+        </Button>
+      </SubmitForm>
+      <div className={s.login__links}>
+        <p>
+          <Link to='/change-pass'>
+            <Button type={btnType.button} variant={btnClass.back}>
+              Забыли пароль?
+            </Button>
+          </Link>
+        </p>
+        <div className={s.login__links_row}>
+          <span>Нет аккаунта?</span>
+          <Button type={btnType.button} variant={btnClass.back}>
+            Зарегистрироваться
+          </Button>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Login;

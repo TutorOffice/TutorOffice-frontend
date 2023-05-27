855d6b1bfe1a5ab3d0928d3aba67b991225e8238
Nickoff <v3136820@gmail.com>
2023-05-27 22:31:35 +0300

    feat: сверстал страницу просмотр учеников + страницу просмотр учеников - пусто (#34)

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import s from './Card.module.css';
 import { btnClass, btnType, Button } from '../../shared/ui';
 
@@ -10,7 +10,27 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ photo, name, subject, subjectLevel }) => {
-  return <div className={s.card}></div>;
+  return (
+    <div className={s.card}>
+      <div className={s.card__wrapper}>
+        <img className={s.card__photo} src={photo} alt='Фото ученика' />
+        <div>
+          <p className={s.card__name}>
+            {name.split(' ')[0]}
+            <br></br>
+            {name.split(' ')[1]}
+          </p>
+          <p className={s.card__subject}>{subject}</p>
+          <p>{subjectLevel}</p>
+        </div>
+      </div>
+      <Link to='#'>
+        <Button type={btnType.button} variant={btnClass.back}>
+          Посмотреть
+        </Button>
+      </Link>
+    </div>
+  );
 };
 
 export default Card;
--- src/pages/studentReview/StudentReview.tsx
+++ src/pages/studentReview/StudentReview.tsx
@@ -7,16 +7,19 @@ import UserInfo from '../../components/userInfo/UserInfo';
 import HeaderMain from '../../components/headerMain/HeaderMain';
 import userPhoto from '../../assets/images/user-photo.jpg';
 import testPhoto from '../../assets/images/test-student.jpg';
+import studentsImage from '../../assets/images/students.png';
 import Card from '../../components/card/Card';
 
-const testStudents = [
+const testStudents: any[] = [
   { id: 1, photo: testPhoto, name: 'Иван Иванов', subject: 'Английский', subjectLevel: 'Уровень - B2' },
-  { id: 2, photo: testPhoto, name: 'Иван Иванов', subject: 'Английский', subjectLevel: 'Уровень - B2' },
-  { id: 3, photo: testPhoto, name: 'Иван Иванов', subject: 'Английский', subjectLevel: 'Уровень - B2' },
-  { id: 4, photo: testPhoto, name: 'Иван Иванов', subject: 'Английский', subjectLevel: 'Уровень - B2' },
-  { id: 5, photo: testPhoto, name: 'Иван Иванов', subject: 'Английский', subjectLevel: 'Уровень - B2' },
-  { id: 6, photo: testPhoto, name: 'Иван Иванов', subject: 'Английский', subjectLevel: 'Уровень - B2' },
+  { id: 2, photo: testPhoto, name: 'Петр Петров', subject: 'Английский', subjectLevel: 'Уровень - А1' },
+  { id: 3, photo: testPhoto, name: 'Денис Денисов', subject: 'Английский', subjectLevel: 'Уровень - B2' },
+  { id: 4, photo: testPhoto, name: 'Евгения Евгеньевна', subject: 'Английский', subjectLevel: 'Уровень - А2' },
+  { id: 5, photo: testPhoto, name: 'Иван Иванов', subject: 'Китайский', subjectLevel: 'Уровень - B2' },
+  { id: 6, photo: testPhoto, name: 'Семен Семенович', subject: 'Английский', subjectLevel: 'Уровень - С1' },
 ];
+
+const dataAvaible: boolean = Boolean(testStudents.length);
 
 const StudentReview = () => {
   return (
@@ -33,25 +36,39 @@ const StudentReview = () => {
         <Button type={btnType.button} variant={btnClass.back} bottom={60}>
           Назад
         </Button>
-        <div className={s.studentReview__title_row}>
-          <h2 className={s.studentReview__title}>Ученики</h2>
-          <Link to='/add-student'>
-            <Button type={btnType.button} variant={btnClass.primary}>
-              Добавить ученика
-            </Button>
-          </Link>
+        <div className={s.studentReview}>
+          <div className={dataAvaible ? s.studentReview__withCards : s.studentReview__blank}>
+            {dataAvaible ? (
+              <h2 className={s.studentReview__title}>Ученики</h2>
+            ) : (
+              <h2 className={s.studentReview__title}>У Вас пока нет ни одного ученика</h2>
+            )}
+            {!dataAvaible && (
+              <p className={s.studentReview__content}>
+                Вы можете добавить нового ученика, указав его фамилию, имя и e-mail.
+              </p>
+            )}
+            <Link to='/add-student'>
+              <Button type={btnType.button} variant={btnClass.primary}>
+                Добавить ученика
+              </Button>
+            </Link>
+          </div>
+          {!dataAvaible && <img className={s.studentReview__image} src={studentsImage} alt='Ученики' />}
         </div>
-        <div className={s.studentReview__cards_container}>
-          {testStudents.map((stud) => (
-            <Card
-              key={stud.id}
-              photo={stud.photo}
-              name={stud.name}
-              subject={stud.subject}
-              subjectLevel={stud.subjectLevel}
-            />
-          ))}
-        </div>
+        {dataAvaible && (
+          <div className={s.studentReview__cards_container}>
+            {testStudents.map((stud) => (
+              <Card
+                key={stud.id}
+                photo={stud.photo}
+                name={stud.name}
+                subject={stud.subject}
+                subjectLevel={stud.subjectLevel}
+              />
+            ))}
+          </div>
+        )}
       </Wrapper>
     </Layout>
   );

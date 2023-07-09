3d9e726a832be70270d97fa7f6fe54aa61e8387a
Nickoff <v3136820@gmail.com>
2023-07-09 15:16:43 +0300

    feat: добавил карточку профиля ученика

--- src/pages/student/Student.tsx
+++ src/pages/student/Student.tsx
@@ -1,9 +1,13 @@
 import s from './Student.module.css';
+import testStudents from './testStudents';
 
 import HeaderAuth from '@/components/headerAuth/HeaderAuth';
+import UserCardFull from '@/components/userCardFull/UserCardFull';
 import Layout from '@/components/layout/Layout';
-
+import studentImage from '@/assets/images/student.png';
 import { Button, btnClass, btnType } from '@/shared/ui';
+
+const stud = testStudents[0];
 
 const Student = () => {
   return (
@@ -13,6 +17,22 @@ const Student = () => {
         Назад
       </Button>
       <h2 className={s.student__title}>Профиль ученика</h2>
+      <div className={s.student__info}>
+        <UserCardFull
+          key={stud.id}
+          studentStatus={stud.activeStatus}
+          photo={stud.photo}
+          name={stud.name}
+          studentSubject={stud.studentSubject}
+          studentSubjectLevel={stud.studentSubjectLevel}
+          email={stud.email}
+          phone={stud.phone}
+          homeWorkDone={stud.homeWorkDone}
+          homeWorkArrowUp={stud.homeWorkArrowUp}
+          homeWorkToDo={stud.homeWorkToDo}
+        />
+        <img className={s.student__image} src={studentImage} alt='Профиль ученика' />
+      </div>
     </Layout>
   );
 };

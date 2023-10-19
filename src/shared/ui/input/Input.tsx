d1081724d74a342dfcaa09755db64944f64c75e2
zeroqs <smthq@bk.ru>
2023-10-19 09:03:59 +0300

    Refactor | ♻️ Input refactor start

--- src/shared/ui/input/Input.tsx
+++ src/shared/ui/input/Input.tsx
@@ -1,140 +1,21 @@
+import { ElementProps, TextInput, TextInputProps } from '@mantine/core'
 import clsx from 'clsx'
-import React, { InputHTMLAttributes, ReactNode } from 'react'
-import { FieldError, FieldValues, UseFormRegister } from 'react-hook-form'
-
-import InputPassword from '@/shared/ui/input/InputPassword/InputPassword'
-import InputPhone from '@/shared/ui/input/InputPhone/InputPhone'
-import { validateType } from '@/shared/validation'
+import React from 'react'
 
 import s from './Input.module.css'
 
-export enum inputTypes {
-  text = 'text',
-  password = 'password',
-  phone = 'phone',
-  email = 'email',
-  button = 'button',
-  textarea = 'textarea',
-}
-
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  type: inputTypes
-  isDisabled?: boolean
-  placeholder?: string
-  labelText?: string
-  commentTip?: string
-  isRequired?: boolean
-  isError?: boolean
-  isPassword?: boolean
-  name?: validateType
-  register?: UseFormRegister<FieldValues>
-  errors?: FieldError | undefined
-  inputRef?: React.RefObject<HTMLInputElement>
+interface InputProps
+  extends TextInputProps,
+    ElementProps<'input', keyof TextInputProps> {
   className?: string
-  children?: ReactNode
-  right?: ReactNode | ReactNode[]
 }
 
-const Input: React.FC<InputProps> = ({
-  inputRef,
-  type,
-  isDisabled = false,
-  placeholder = '',
-  labelText = '',
-  commentTip = '',
-  errors,
-  isRequired = false,
-  isError = Boolean(errors),
-  name,
-  register,
-  className,
-  children,
-  right,
-  value,
-  ...props
-}) => {
-  const classNameInput = clsx(className, {
+const Input: React.FC<InputProps> = ({ className, ...props }) => {
+  const classes = clsx(className, {
     [s.input]: true,
-    [s.textarea]: type === inputTypes.textarea,
-    [s.error]: isError,
   })
-  const classNameTip = clsx({
-    [s.commentTip]: true,
-    [s.errorTip]: isError,
-  })
-  if (type === inputTypes.password) {
-    return (
-      <InputPassword
-        inputRef={inputRef}
-        isDisabled={isDisabled}
-        placeholder={placeholder}
-        labelText={labelText}
-        commentTip={commentTip}
-        isRequired={isRequired}
-        isError={isError}
-        name={name}
-        register={register}
-        className={className}
-        {...props}
-      />
-    )
-  }
 
-  if (type === inputTypes.phone) {
-    return (
-      <InputPhone
-        inputRef={inputRef}
-        isDisabled={isDisabled}
-        placeholder={placeholder}
-        labelText={labelText}
-        commentTip={commentTip}
-        isRequired={isRequired}
-        isError={isError}
-        name={name}
-        register={register}
-        className={className}
-        {...props}
-      />
-    )
-  }
-  return (
-    <div className={s.inputs}>
-      <label>
-        <p className={s.labelText}>
-          {labelText}
-          {isRequired && <span className={s.required__icon}>*</span>}
-        </p>
-      </label>
-      <div className={s.inputBlock}>
-        <div>
-          {type === inputTypes.textarea ? (
-            <textarea
-              {...(register && name && { ...register(name) })}
-              placeholder={placeholder}
-              className={classNameInput}
-            />
-          ) : (
-            <input
-              value={value}
-              ref={inputRef}
-              {...(register && name && { ...register(name) })}
-              type={type}
-              placeholder={placeholder}
-              disabled={isDisabled}
-              className={classNameInput}
-              {...props}
-            />
-          )}
-        </div>
-        <div className={s.right}>
-          <div className={s.rightCentered}>{right}</div>
-        </div>
-        {children}
-      </div>
-      {errors && <p className={s.required}>{errors.message}</p>}
-      <p className={classNameTip}>{commentTip}</p>
-    </div>
-  )
+  return <TextInput className={classes} {...props} />
 }
 
 export default Input

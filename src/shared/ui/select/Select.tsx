c8f3420af04032f4f2596e8852f6c1cadc813cff
zeroqs <smthq@bk.ru>
2023-10-20 04:55:15 +0300

    Refactor | ♻️ Select base done

--- src/shared/ui/select/Select.tsx
+++ src/shared/ui/select/Select.tsx
@@ -1,122 +1,40 @@
-import clsx from 'clsx'
-import React, { ChangeEvent, useEffect, useRef, useState } from 'react'
 import {
-  Control,
-  Controller,
-  FieldValues,
-  UseFormRegister,
-} from 'react-hook-form'
+  ElementProps,
+  Select as MantSelect,
+  SelectProps as MantSelectProps,
+} from '@mantine/core'
+import React from 'react'
 
-import { ReactComponent as Icon } from '@/assets/icons/select-down.svg'
-import { Input } from '@/shared/ui'
-import Options from '@/shared/ui/select/Options/Options'
-import { validateType } from '@/shared/validation'
+import { ReactComponent as ArrowIcon } from '@/assets/icons/select-down.svg'
 
 import s from './Select.module.css'
 
-interface Option {
-  readonly label: string
-  readonly value: string
-}
-
-interface ISelect {
-  options?: Option[]
-  placeholder?: string
+interface SelectProps
+  extends MantSelectProps,
+    ElementProps<'input', keyof MantSelectProps> {
   className?: string
-  name?: validateType
-  register?: UseFormRegister<FieldValues>
-  control?: Control<FieldValues>
 }
 
-const initialSelectOptions = [{ label: 'Виноград 🍇', value: '🍇' }]
+const classNames = {
+  input: s.input,
+  label: s.label,
+  error: s.error,
+  description: s.description,
+  wrapper: s.wrapper,
+  option: s.option,
+  options: s.options,
+  dropdown: s.dropdown,
+}
 
-const Select = ({
-  options = initialSelectOptions,
-  placeholder,
-  className,
-  name = validateType.firstName,
-  register,
-  control,
-}: ISelect) => {
-  const [showOptions, setShowOptions] = useState(false)
-  const [optionValue, setOptionValue] = useState<string>('')
-
-  const classNameOptions = clsx(s.hide, {
-    [s.showOptions]: showOptions,
-    [s.closeOptions]: !showOptions,
-  })
-
-  const classNameSelect = clsx(className, s.input, {
-    [s.inputFocus]: showOptions,
-  })
-
-  const ref = useRef<HTMLDivElement>(null)
-  const inputRef = useRef<HTMLInputElement>(null)
-
-  const handlerOptionValue = (event: ChangeEvent<HTMLInputElement>) => {
-    setOptionValue(event.target.value)
-  }
-
-  const handlerOptions = () => {
-    setShowOptions(!showOptions)
-    inputRef.current?.focus()
-  }
-
-  useEffect(() => {
-    const checkIfClickedOutside = (e: TouchEvent | MouseEvent) => {
-      if (
-        showOptions &&
-        ref.current &&
-        !ref.current.contains(e.target as Node)
-      ) {
-        setShowOptions(false)
-      }
-    }
-    document.addEventListener('mousedown', checkIfClickedOutside)
-    return () => {
-      document.removeEventListener('mousedown', checkIfClickedOutside)
-    }
-  }, [showOptions])
-
-  useEffect(() => {
-    if (showOptions) {
-      setOptionValue('')
-    }
-  }, [showOptions])
-
+const Select: React.FC<SelectProps> = ({ className, ...props }) => {
   return (
-    <div ref={ref} className={s.root} onClick={handlerOptions}>
-      <Controller
-        name={name}
-        control={control}
-        render={({ field }) => (
-          <Input
-            register={register}
-            name={name}
-            inputRef={inputRef}
-            value={optionValue}
-            onChange={handlerOptionValue}
-            placeholder={placeholder}
-            className={classNameSelect}
-            type={inputTypes.text}
-            right={
-              <button type="button" className={classNameOptions}>
-                <Icon />
-              </button>
-            }
-          >
-            {showOptions && (
-              <Options
-                optionValue={optionValue}
-                options={options}
-                setHookFormValue={field.onChange}
-                setOptionValue={setOptionValue}
-              />
-            )}
-          </Input>
-        )}
-      />
-    </div>
+    <MantSelect
+      withCheckIcon={false}
+      rightSection={<ArrowIcon />}
+      classNames={classNames}
+      className={className}
+      {...props}
+    />
   )
 }
 
